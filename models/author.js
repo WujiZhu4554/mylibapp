655a9ce3c29615dc7c455ab5c3f08b2eaadaa9f3
Wuji ZHU <wujiz1@student.unimelb.edu.au>
2020-04-21 23:29:45 +1000

    a

--- models/author.js
+++ models/author.js
@@ -3,6 +3,8 @@ const authorSchema = new mongoose.Schema({
 id: String,
 first_name: String,
 last_name: String
+},{
+	versionKey: false
 });
 const Author = mongoose.model("author", authorSchema, "author");
 module.exports = Author;
