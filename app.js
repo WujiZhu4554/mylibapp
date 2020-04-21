7d5a8c0f5e861a1e013514e67dfb83b1c0777c65
Wuji ZHU <wujiz1@student.unimelb.edu.au>
2020-04-21 21:37:38 +1000

    a

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
-// set up author routes
-const authorRouter = require('./routes/authorRouter');
+
+require("./models");
+
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // GET home page
-app.get('/', (req, res) => {
-res.send('<H1>Library System</H1>')
+app.get("/", (req, res) => {
+  res.send("<H1>Library System</H1>");
 });
-// Handle author-management requests
+
+// handle author-management related requests
+// first import the author router
+const authorRouter = require("./routes/authorRouter");
+
 // the author routes are added onto the end of '/author-management'
-app.use('/author-management', authorRouter);
-app.listen(process.env.PORT, () => {
-console.log("The library app is running!")
+app.use("/author-management", authorRouter);
+
+// start app and listen for incoming requests on port
+app.listen(process.env.PORT || 3000, () => {
+  console.log("The library app is running!");
 });
+
